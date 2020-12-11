bb62898f7309e3215678e2bb739d1f2c55224528
acieagsalud <aciejohn@gmail.com>
2020-12-11 00:10:36 -0600

    Added withAuthenticator to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+// @ts-ignore
+import Amplify, { Auth } from 'aws-amplify';
+// @ts-ignore
+import awsconfig from './aws-exports';
+// @ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native'
+
+Amplify.configure(awsconfig);
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
+export default withAuthenticator(App, {includeGreetings: true});
+
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
